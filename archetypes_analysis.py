0799400fba7f96b7edfbc341d626280f670cf552
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2023-07-09 16:40:02 -0400

    pca function added

--- archetypes_analysis.py
+++ archetypes_analysis.py
@@ -5,3 +5,9 @@ def get_archetypes(data):
 
 
 
+# X = np.random.normal(0, 1, (100, 2))
+
+# aa = arch.AA(n_archetypes=4)
+
+# X_trans = aa.fit_transform(X)
+# print(X_trans.shape)
--- main.py
+++ main.py
@@ -1,63 +1,44 @@
 import pandas as pd
-import sklearn
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
-import archetypes as arch
 from archetypes_analysis import get_archetypes
+import matplotlib as plt
 
-X = np.random.normal(0, 1, (100, 2))
-
-aa = arch.AA(n_archetypes=4)
-
-X_trans = aa.fit_transform(X)
-print(X_trans.shape)
 
 data = pd.read_csv("dataset.csv")
+data = data.apply(LabelEncoder().fit_transform)
+# 448 columns / dimensions -- reduce this to 8
+# 80.49% of our variance
+print(data.shape)
+
+X = data.values
+print(X.shape)
+
+pca_448 = PCA(n_components=8, random_state=2023)
+pca_448.fit(X)
+X_pca_448 = pca_448.transform(X)
+
+# print(X_pca_448.shape)
+# print(sum(pca_448.explained_variance_ratio_ * 100))
+
+print(X_pca_448)
+
+
+
 
 # step 1 - PCA 
 # Principal Component Analysis to find 8 principal components
 # TODO: Mouayad and Natasha
 
-def PCA():
     # ------ implement it here ------
-def PCA(data, dims_rescaled_data=2):
-    """
-    returns: data transformed in 2 dims/columns + regenerated original data
-    pass in: data as 2D NumPy array
-    """
-    import numpy as NP
-    from scipy import linalg as LA
-    m, n = data.shape
-    # mean center the data
-    data -= data.mean(axis=0)
-    # calculate the covariance matrix
-    R = NP.cov(data, rowvar=False)
-    # calculate eigenvectors & eigenvalues of the covariance matrix
-    # use 'eigh' rather than 'eig' since R is symmetric, 
-    # the performance gain is substantial
-    evals, evecs = LA.eigh(R)
-    # sort eigenvalue in decreasing order
-    idx = NP.argsort(evals)[::-1]
-    evecs = evecs[:,idx]
-    # sort eigenvectors according to same index
-    evals = evals[idx]
-    # select the first n eigenvectors (n is desired dimension
-    # of rescaled data array, or dims_rescaled_data)
-    evecs = evecs[:, :dims_rescaled_data]
-    # carry out the transformation on the data using eigenvectors
-    # and return the re-scaled data, eigenvalues, and eigenvectors
-    return NP.dot(evecs.T, data.T).T, evals, evecs
+# pca = decomposition.PCA(n_components=3)
+# pca.fit(X)
+# X = pca.transform(X)
 
-def test_PCA(data, dims_rescaled_data=2):
-    '''
-    test by attempting to recover original data array from
-    the eigenvectors of its covariance matrix & comparing that
-    'recovered' array with the original data
-    '''
-    _ , _ , eigenvectors = PCA(data, dim_rescaled_data=2)
-    data_recovered = NP.dot(eigenvectors, m).T
-    data_recovered += data_recovered.mean(axis=0)
-    assert NP.allclose(data, data_recovered)
-    
+# print(X.shape)
+
+
 
 def plot_pca(data):
     from matplotlib import pyplot as MPL
@@ -72,19 +53,19 @@ def plot_pca(data):
 
 
 
->>> df = "~/keratoconus dataset.csv"
->>> data = NP.loadtxt(df, delimiter=',')
->>> # remove class labels
->>> data = data[:,:-1]
->>> plot_pca(data)
+# >>> df = "~/keratoconus dataset.csv"
+# >>> data = NP.loadtxt(df, delimiter=',')
+# >>> # remove class labels
+# >>> data = data[:,:-1]
+# >>> plot_pca(data)
 
 
 
 
-    pass
+    # pass
 
 
------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # step 2 - Archetypal Analysis
@@ -94,7 +75,7 @@ def plot_pca(data):
 # I'm doing this in another file (`archetypes_analysis.py`) and will import it here.
 
 
------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # step 3 - Create a residual sum of squares curve for all archetypes.
@@ -107,14 +88,14 @@ def residual_sum_of_squares(archetypes):
     # ------ implement it here ------
     pass
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # step 4 - Use elbow method or silhouette method to find most appropriate archetypal model.
 # TODO: All of us
 
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # step 5 - repeat the above,except, before starting, hold out 20% of the data. 
